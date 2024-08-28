c5b48cf7003cd5fd4183eef338e0ec3ecd56523b
llSiddharthll <siddharthgreat443@gmail.com>
2024-08-28 22:18:41 +0530

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
+
+# Ensure pip is available
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found, installing..."
+    python -m ensurepip --upgrade
+    python -m pip install --upgrade pip
+fi
+
+# Install dependencies
 pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+
+# Collect static files (if necessary)
+python manage.py collectstatic --noinput
