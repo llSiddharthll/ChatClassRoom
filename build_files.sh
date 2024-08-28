df0fd404d3add54cc2632489b2e885b2e8aa040f
llSiddharthll <siddharthgreat443@gmail.com>
2024-08-28 22:27:38 +0530

    done

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
-
-# Ensure pip is available
-if ! command -v pip &> /dev/null
-then
-    echo "pip could not be found, installing..."
-    python -m ensurepip --upgrade
-    python -m pip install --upgrade pip
-fi
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files (if necessary)
-python manage.py collectstatic --noinput
+echo "BUILD START"
+pip install --upgrade pip
+pip3 install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+echo "BUILD DONE"
